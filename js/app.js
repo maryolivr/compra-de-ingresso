f947ccb88337857e83615cfb7c4265cf073cceaf
Mary Oliveira <maryolivr@gmail.com>
2025-09-18 10:20:40 -0300

    Adicionando a função comprar ingresso

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function comprar() {
+   let tipo = document.getElementById('tipo-ingresso');
+   let qtd = parseInt(document.getElementById('qtd').value);
+
+   if(tipo.value == 'pista') {
+        comprarPista(qtd);
+   }
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para tipo pista');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso');
+    }
+}
